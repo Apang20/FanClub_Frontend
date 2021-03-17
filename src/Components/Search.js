64aa445e7374cd7e4f36d63cf718426ef4291edd
Avelon <62185859+Apang20@users.noreply.github.com>
2021-03-17 17:16:26 -0500

    Update Search.js

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,3 @@
-// import React, { Component } from 'react'
 import { Select } from "@chakra-ui/react"
 import React from "react";
 
@@ -28,74 +27,3 @@ const Search = (props) => {
 };
 
 export default Search;
-// <img width="8%" height="10%" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ccaa6b40-c9e1-4fca-a714-e73eb7a0d47d/d998yjs-cbde5b99-d8ed-409d-97a3-e2dc4807c12b.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvY2NhYTZiNDAtYzllMS00ZmNhLWE3MTQtZTczZWI3YTBkNDdkXC9kOTk4eWpzLWNiZGU1Yjk5LWQ4ZWQtNDA5ZC05N2EzLWUyZGM0ODA3YzEyYi5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.jOOwRQ9Nuo_hoherDGrCkFWIcSqg2wL9C0XevdVXKyU"/>
-
-
-// <select size="large" width="100%" className="select" onChange={(e) => props.updateFilter(e.target.value)}>
-//         </select>
-
-// <Autocomplete
-//   id="combo-box-demo"
-//   options={top100Films}
-//   getOptionLabel={(option) => option.title}
-//   style={{ width: 300 }}
-//   renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-// />
-
-
-
-
-
-
-
-//   <label>
-//   <strong>Filter By Ghibli Movies:</strong> <br></br>
-//   <select onChange={(e) => props.updateFilter(e.target.value)}>
-//     <option value="All">All</option>
-//     <option value="Ghibli">Ghibli</option>
-//     <option value="My Nieghbor Totoro">My Nieghbor Totoro</option>
-//     <option value="Ponyo">Ponyo</option>
-//     <option value="Princess Mononoke">Princess Mononoke</option>
-//     <option value="Howls Moving Castle">Howls Moving Castle</option>
-//     <option value="Kiki's Delivery Service">Kiki's Delivery Service</option>
-//     <option value="Spirited Away">Spirited Away</option>
-//   </select>
-// </label>
-
-
-{/*<label>
-        <strong>Search Stocks:</strong>
-        <input onChange={(e) => props.updateSearch(e.target.value)} />
-      </label> 
-<br></br>
-      <strong>Sort:</strong>
-      <label>
-        <input
-          type="radio"
-          value="Alphabetically"
-          checked={props.sort === "Alphabetically"}
-          onChange={(e) => props.updateSort(e.target.value)}
-        />
-        Alphabetically
-      </label>
-      <label>
-        <input
-          type="radio"
-          value="Price"
-          checked={props.sort === "Price"}
-          onChange={(e) => props.updateSort(e.target.value)}
-        />
-        Price
-      </label>
-      <label>
-        <input
-          type="radio"
-          value="None"
-          checked={props.sort === "None"}
-          onChange={(e) => props.updateSort(e.target.value)}
-        />
-        None
-      </label>
-      <br /> 
-*/}
-
