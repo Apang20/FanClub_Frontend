4beb51e5c3ceb4cdfce1fa67e5adc541b9c19b8f
Avelon <62185859+Apang20@users.noreply.github.com>
2021-03-17 17:06:38 -0500

    Update MoreButton.js

--- src/Components/MoreButton.js
+++ src/Components/MoreButton.js
@@ -1,9 +1,6 @@
-// import React from 'react'
 import Button from '@material-ui/core/Button'
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward'
 import React from 'react'
-
-
 
 const MoreButton = (props) => {
   return( <div>
@@ -15,21 +12,3 @@ const MoreButton = (props) => {
 }
 
   export default MoreButton;
-
-
-
-
-
-
-
-// const MoreButton = (props) => {
-//   return 
-//   <div>
-//     <Button startIcon={<SkipNextIcon/>}
-//      variant="contained" 
-//      color="inherit" 
-//      onClick={props.moreItems}>
-//     </Button>
-//   </div>
-// }
-// export default MoreButton
