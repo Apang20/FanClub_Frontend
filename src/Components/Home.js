d3d0e38ec30b1fa291566ed439e2a1a95f6ba433
Avelon <62185859+Apang20@users.noreply.github.com>
2021-03-17 16:51:05 -0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,11 +11,9 @@ import Copyright from '../Components/Copyright'
 const styles = theme => ({
   mainFeaturedPost: {
     position: 'relative',
-    // backgroundColor: theme.palette.grey[800],
     color: theme.palette.common.white,
     marginBottom: theme.spacing(2),
     marginTop: theme.spacing(1),
-    //backgroundImage: 'url(https://i.imgur.com/VYBxMKp.gif)',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
@@ -32,8 +30,7 @@ const styles = theme => ({
     position: 'relative',
     padding: theme.spacing(0),
     [theme.breakpoints.up('md')]: {
-      padding: theme.spacing(11), //zooms in on pic
-    //   padding: theme.spacing(40),
+      padding: theme.spacing(11), 
       paddingRight: 0,
     },
   },
@@ -50,15 +47,10 @@ return (
 <div align="right" >
 <img width="9%" height="1%" src="https://media2.giphy.com/media/dZiikBDkgHfRvfdHZ9/source.gif"/>
 </div>
-{/* Increase the priority of the hero background image */}
 {<img style={{ display: 'none' }} src={`url(https://data.whicdn.com/images/159503389/original.gif)`} alt={classes.imageText} />}
 {/**/}<div className={classes.overlay} />
 <br/>
 <br/>
-
-{/*<div align="left" >
-<img width="10%" height="1%" src="https://media2.giphy.com/media/dZiikBDkgHfRvfdHZ9/source.gif"/>
-</div>*/}
 <Grid container>
 <Grid item md={6}>
 <div className={classes.mainFeaturedPostContent}>
@@ -82,63 +74,3 @@ Here, exclusive members can find rare collection of Ghibli items imported from T
 
 export default withStyles(styles)(Home)
 
-
-// <Paper className={classes.mainFeaturedPost} style={{ backgroundImage: `url(https://cdn.shopify.com/s/files/1/1549/8499/products/il_fullxfull.900638191_iqit_1024x1024.jpg?v=1478316546)`
-// <Paper className={classes.mainFeaturedPost} style={{ backgroundImage: `url(https://i.pinimg.com/564x/eb/fe/12/ebfe125f3f4bdc3b5d3505349f3a7aaa.jpg)`
-// <Paper className={classes.mainFeaturedPost} style={{ backgroundImage: `url(https://media.giphy.com/media/3ohzdHkYXGiemtvCMM/giphy.gif)`
-
-// <Paper className={classes.mainFeaturedPost} style={{ backgroundImage: `url(https://media.giphy.com/media/YfnALOssX8C1W/giphy.gif)`
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, {Component} from 'react';
-// import { withStyles } from "@material-ui/core/styles";
-
-// const styles = theme => ({
-// });
-
-
-// class Home extends Component {
-// render() {
-//     const {classes} = this.props
-//     return (
-
-//         );
-//     }
-// }
-
-
-// export default withStyles(styles)(Home)
-
-
-
-
-
-// import React, { Component } from "react";
-// class home extends Component {
-//     render() {
-//         return (
-//             <div className="home">
-//                 <img className="home-pic" src="https://i0.wp.com/prpldrop.com/wp-content/uploads/2020/09/https___hypebeast.com_image_2020_04_studio-ghibli-free-wallpaper-download-info-01.jpg?w=1170&ssl=1" />
-
-               
-//                 </div>
-//         );
-//     }
-// }
-
-// export default home;
-
-// <div className="home-paragraph">
-// <img className="background-pic" src="https://i.pinimg.com/originals/4a/53/8c/4a538c7eb58d4d833a08673776e8eb3f.gif" />
